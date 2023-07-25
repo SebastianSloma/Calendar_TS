8b05734d366f6ca02b42e038eda6dc8327f43ef0
Sebastian <sebastiansloma@gmail.com>
2023-07-25 11:46:20 +0200

    datepicket add container

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,1 +1,9 @@
 import React from "react";
+
+export const DatePicker: React.FC<{}> = ({}) =>{
+return(
+    <div className="date-picker-container">
+        <div className="background-continer"></div>
+    </div>
+)
+}

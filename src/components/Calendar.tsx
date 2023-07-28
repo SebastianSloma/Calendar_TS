d7c0ddfb7935b7c592653a231332460e531df4f3
Sebastian <sebastiansloma@gmail.com>
2023-07-28 12:52:04 +0200

    added calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {Weekday, Date} from '../../types'
+import { Weekdays } from "../configs/Weekdays";
 
 export const Calendar: React.FC<{}> =({}) => {
     return(
@@ -6,8 +8,13 @@ export const Calendar: React.FC<{}> =({}) => {
             <div className="datepicker-container">
 
             </div>
+            <div className="weekdays-container">
+                {Weekdays.map(day =>(
+                    <div className="week-day">{day}</div>
+                ))}
+            </div>
             <div className="calendar">
-                
+
             </div>
 
         </div>
--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Calendar } from "./Calendar";
 
 // eslint-disable-next-line no-empty-pattern
 export const DatePicker: React.FC<{}> = ({}) =>{
 return(
     <div className="date-picker-container">
         <div className="background-container"></div>
+        <Calendar/>
     </div>
 )
 }

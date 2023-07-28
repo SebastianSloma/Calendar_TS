09f38afe8d502c341ec8955567fb58676bc9d7bb
Sebastian <sebastiansloma@gmail.com>
2023-07-28 12:43:49 +0200

    added types.ts

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Calendar: React.FC<{}> =({}) => {
+    return(
+        <div className="calendar-container">
+            <div className="datepicker-container">
+
+            </div>
+            <div className="calendar">
+                
+            </div>
+
+        </div>
+    )
+}
--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
+// eslint-disable-next-line no-empty-pattern
 export const DatePicker: React.FC<{}> = ({}) =>{
 return(
     <div className="date-picker-container">
-        <div className="background-continer"></div>
+        <div className="background-container"></div>
     </div>
 )
 }

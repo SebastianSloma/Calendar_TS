7ffd42e71d0fe1707eb4c113380708b110304dc7
Sebastian <sebastiansloma@gmail.com>
2023-07-31 10:14:31 +0200

    added days

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -4,6 +4,22 @@ import { monthDates } from "../configs/MonthDays";
 import { Weekdays } from "../configs/Weekdays";
 
 export const Calendar: React.FC<{}> =({}) => {
+
+    const generateDates = (date:number) =>{
+        for(let i=0;i<7;i++){
+            return<button className="date" value={date}><p>{date}</p></button>
+        }
+    }
+
+    const generateWeeks = (dates: Array<Date>) =>{
+        let daysInWeek = 7
+        let tempArray = []
+
+        for (let i=0; i < dates.length; i+= daysInWeek){
+            tempArray.push(dates.slice(i, i+daysInWeek))
+        }
+        return tempArray
+        }
     return(
         <div className="calendar-container">
             <div className="datepicker-container">
@@ -15,7 +31,12 @@ export const Calendar: React.FC<{}> =({}) => {
                 ))}
             </div>
             <div className="calendar">
-
+                {generateWeeks(monthDates).map(week =>(
+                    <div className="week">
+                        {week.map(day=>(generateDates(day.day)))}
+                    </div>
+))
+} 
             </div>
 
         </div>

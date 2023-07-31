4a870131b72035b60c5a4f9f028076c4ebbcd753
Sebastian <sebastiansloma@gmail.com>
2023-07-31 09:56:51 +0200

    import monthdate

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Weekday, Date} from '../../types'
+import { monthDates } from "../configs/MonthDays";
 import { Weekdays } from "../configs/Weekdays";
 
 export const Calendar: React.FC<{}> =({}) => {

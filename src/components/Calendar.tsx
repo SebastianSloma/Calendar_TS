714428472fe6d2ef0bf3ed1b8a4d1047c1e21702
Sebastian <sebastiansloma@gmail.com>
2023-08-03 11:51:59 +0200

    added icons

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,44 +1,45 @@
-import React from "react";
-import {Weekday, Date} from '../../types'
-import { monthDates } from "../configs/MonthDays";
-import { Weekdays } from "../configs/Weekdays";
+import React from 'react';
+import { Weekday, Date } from '../../types';
+import { monthDates } from '../configs/MonthDays';
+import { Weekdays } from '../configs/Weekdays';
 
-export const Calendar: React.FC<{}> =({}) => {
+export const Calendar: React.FC<{}> = ({}) => {
+	const generateDates = (date: number) => {
+		for (let i = 0; i < 7; i++) {
+			return (
+				<button className='date' value={date}>
+					<p>{date}</p>
+				</button>
+			);
+		}
+	};
 
-    const generateDates = (date:number) =>{
-        for(let i=0;i<7;i++){
-            return<button className="date" value={date}><p>{date}</p></button>
-        }
-    }
+	const generateWeeks = (dates: Array<Date>) => {
+		let daysInWeek = 7;
+		let tempArray = [];
 
-    const generateWeeks = (dates: Array<Date>) =>{
-        let daysInWeek = 7
-        let tempArray = []
-
-        for (let i=0; i < dates.length; i+= daysInWeek){
-            tempArray.push(dates.slice(i, i+daysInWeek))
-        }
-        return tempArray
-        }
-    return(
-        <div className="calendar-container">
-            <div className="datepicker-container">
-
+		for (let i = 0; i < dates.length; i += daysInWeek) {
+			tempArray.push(dates.slice(i, i + daysInWeek));
+		}
+		return tempArray;
+	};
+	return (
+		<div className='calendar-container'>
+			<div className='datepicker-container'>
+                <span>February 2023</span>
             </div>
-            <div className="weekdays-container">
-                {Weekdays.map(day =>(
-                    <div className="week-day">{day}</div>
-                ))}
-            </div>
-            <div className="calendar">
-                {generateWeeks(monthDates).map(week =>(
-                    <div className="week">
-                        {week.map(day=>(generateDates(day.day)))}
-                    </div>
-))
-} 
-            </div>
-
-        </div>
-    )
-}
+			<div className='weekdays-container'>
+				{Weekdays.map((day) => (
+					<div className='week-day'>{day}</div>
+				))}
+			</div>
+			<div className='calendar'>
+				{generateWeeks(monthDates).map((week) => (
+					<div className='week'>
+						{week.map((day) => generateDates(day.day))}
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
